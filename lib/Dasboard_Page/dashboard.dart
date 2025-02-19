1af3f80cee8566f90819828f1e5de22c8387f075
Typicalsleepingboy <typicalsleepingboyxxx@gmail.com>
2025-02-20 00:42:12 +0700

    Feat: Update ui card bottomnavbar

--- lib/Dasboard_Page/dashboard.dart
+++ lib/Dasboard_Page/dashboard.dart
@@ -124,50 +124,59 @@ class DashboardScreen extends StatelessWidget {
           ),
 
           Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
+            bottom: 16, 
+            left: 16,  
+            right: 16,  
             child: ClipRRect(
+              borderRadius: BorderRadius.circular(30), 
               child: BackdropFilter(
-                filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
                 child: Container(
                   decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.8),
-                    border: Border(
-                      top: BorderSide(
-                        color: Colors.grey.withOpacity(0.2),
-                        width: 1,
-                      ),
-                    ),
-                  ),
-                  child: BottomNavigationBar(
-                    backgroundColor: Colors.transparent,
-                    elevation: 0,
-                    type: BottomNavigationBarType.fixed,
-                    selectedItemColor: const Color(0xFF34a853),
-                    unselectedItemColor: Colors.grey,
-                    items: const [
-                      BottomNavigationBarItem(
-                        icon: Icon(Icons.home),
-                        label: 'Home',
-                      ),
-                      BottomNavigationBarItem(
-                        icon: Icon(Icons.article),
-                        label: 'Activity',
-                      ),
-                      BottomNavigationBarItem(
-                        icon: Icon(Icons.qr_code_scanner),
-                        label: 'Scan',
-                      ),
-                      BottomNavigationBarItem(
-                        icon: Icon(Icons.bar_chart),
-                        label: 'Statistics',
-                      ),
-                      BottomNavigationBarItem(
-                        icon: Icon(Icons.person),
-                        label: 'Profile',
+                    color: Colors.white54,
+                    borderRadius: BorderRadius.circular(30),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.black12,
+                        blurRadius: 10,
+                        offset: Offset(0, 4),
                       ),
                     ],
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: BottomNavigationBar(
+                      backgroundColor: Colors.transparent,
+                      elevation: 0,
+                      type: BottomNavigationBarType.fixed,
+                      selectedItemColor: const Color(0xFF34a853),
+                      unselectedItemColor: Colors.grey,
+                      showUnselectedLabels: true,
+                      selectedFontSize: 12,
+                      unselectedFontSize: 12,
+                      items: const [
+                        BottomNavigationBarItem(
+                          icon: Icon(Icons.home),
+                          label: 'Home',
+                        ),
+                        BottomNavigationBarItem(
+                          icon: Icon(Icons.article),
+                          label: 'Activity',
+                        ),
+                        BottomNavigationBarItem(
+                          icon: Icon(Icons.qr_code_scanner),
+                          label: 'Scan',
+                        ),
+                        BottomNavigationBarItem(
+                          icon: Icon(Icons.bar_chart),
+                          label: 'Statistics',
+                        ),
+                        BottomNavigationBarItem(
+                          icon: Icon(Icons.person),
+                          label: 'Profile',
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
@@ -182,7 +191,7 @@ class DashboardScreen extends StatelessWidget {
     return Container(
       decoration: BoxDecoration(
         color: Colors.white,
-        borderRadius: BorderRadius.circular(12),
+        borderRadius: BorderRadius.circular(30),
         boxShadow: [
           BoxShadow(
             color: Colors.grey.withOpacity(0.1),
@@ -220,7 +229,7 @@ class DashboardScreen extends StatelessWidget {
       padding: const EdgeInsets.all(16),
       decoration: BoxDecoration(
         color: Colors.white,
-        borderRadius: BorderRadius.circular(12),
+        borderRadius: BorderRadius.circular(30),
         boxShadow: [
           BoxShadow(
             color: Colors.grey.withOpacity(0.1),
